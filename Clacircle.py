9f472643beaaf9fa30130e79b3b4547999377682
Yuvraj Chavan <149896265+Chavanyuvraj@users.noreply.github.com>
2024-09-25 09:56:56 +0530

    Clacircle.py

---
+++ Clacircle.py
@@ -0,0 +1,13 @@
+class circle ():
+   def _init__(self,r):
+       self.radius=r
+   def area(self):
+      return self.radius*r*3.14
+   def perimeter(self):
+
+      return 2*self.radius*3.14
+newcircle=circle (8)
+
+print(new circle.area())
+
+print(newcircle.perimeter())
